b7843a60be85f22825766c1e23448ba16f9a0f4d
leoboyerbx <contact@leoboyer.fr>
2023-03-03 14:29:29 +0100

    Ionic Hello world

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/ionic',
+        'nuxt-windicss',
+    ],
     ssr: false,
     devServer: {
         port: 4000
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import HelloJourniz from '~/components/fake/HelloJourniz.vue';
+</script>
+<template>
+<ion-page class="bg-gray-300 flex flex-col items-center">
+  <HelloJourniz />
+</ion-page>
+</template>
