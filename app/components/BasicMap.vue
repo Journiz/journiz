9c9d048bc27ff56a8dc6d51a5c338a2a29af6798
Vincent Creton <v.creton@herewecom.fr>
2023-03-08 23:55:43 +0100

    Install @studiometa/vue-mapbox-gl and use this to display map

--- app/components/BasicMap.vue
+++ app/components/BasicMap.vue
@@ -1,26 +1,15 @@
 <script lang="ts" setup>
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from "mapbox-gl";
-import {onMounted} from "#imports";
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiY3JldG9udiIsImEiOiJjbGV5b2Fld2QwNnh4M3JvOGIxNHZ5a3VkIn0.WdHz6eP4SsoCqMuejCRpRg';
-onMounted(() => {
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-  });
-})
-
+import { MapboxMap } from '@studiometa/vue-mapbox-gl';
 </script>
 <template>
-  <div id="map" class="w-full h-full"></div>
+  <MapboxMap
+      class="w-full h-full absolute top-0 left-0"
+      access-token="pk.eyJ1IjoiY3JldG9udiIsImEiOiJjbGV5b2Fld2QwNnh4M3JvOGIxNHZ5a3VkIn0.WdHz6eP4SsoCqMuejCRpRg"
+      map-style="mapbox://styles/mapbox/streets-v12"
+      :center="[6.129384, 45.899247]"
+      :zoom="13"
+  />
+
 </template>
 <style scoped>
-#map {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 3rem;
-  padding: 10rem;
-}
 </style>
