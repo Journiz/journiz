d2ce92658fe1518cf476c5f32818ef5d5df6445e
Vincent Creton <v.creton@herewecom.fr>
2023-03-09 18:51:45 +0100

    FIX: Temporary fix to the impossibility to move the camera on position watching

--- app/components/BasicMap.vue
+++ app/components/BasicMap.vue
@@ -1,8 +1,19 @@
 <script lang="ts" setup>
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl';
+import {reactive} from "vue";
 // let positionMarkerCoord = [6.129384, 45.899247]
 defineProps(['positionMarkerCoord'])
+
+let currentCenter = reactive({lng: -122.02703037000001, lat: 37.330208800000001})
+let map:MapboxMap = null
+
+onMounted(() => {
+  // Catch new position of center after dragging
+  map.on('drag', () => {
+    currentCenter = map.getCenter()
+  });
+})
 
 </script>
 <template>
@@ -10,8 +21,9 @@ defineProps(['positionMarkerCoord'])
       class="w-full h-full absolute top-0 left-0"
       access-token="pk.eyJ1IjoiY3JldG9udiIsImEiOiJjbGV5b2Fld2QwNnh4M3JvOGIxNHZ5a3VkIn0.WdHz6eP4SsoCqMuejCRpRg"
       map-style="mapbox://styles/mapbox/streets-v12"
-      :center="[-122.02703037000001, 37.330208800000001]"
-      :zoom=14>
+      :center="[currentCenter.lng, currentCenter.lat]"
+      :zoom=14
+      @mb-created="(mapInstance) => map = mapInstance" >
     <MapboxMarker :lng-lat="positionMarkerCoord" />
   </MapboxMap>
 
