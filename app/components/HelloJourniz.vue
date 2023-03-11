ebb0176b74590bae71a7084b99dac4f190bf181e
leoboyerbx <contact@leoboyer.fr>
2023-03-11 18:20:44 +0100

    wip: on data composables

--- app/components/HelloJourniz.vue
+++ app/components/HelloJourniz.vue
@@ -7,13 +7,14 @@ const increment = () => count.value++
 // const { data: journey } = useRealtimeJourney('5mk4b6z2819n3x8')
 
 const {data: trip} = useRealtimeTrip('cc790mc7p9ywe2a')
+
 </script>
 <template>
   <div class="flex flex-col items-center">
     <h1>Journiz</h1>
-    <p v-if="trip">
+    <div v-if="trip">
       <pre>{{ trip }}</pre>
-    </p>
+    </div>
     <p>Count is: <strong>{{ count }}</strong>, doubleCount is <strong class="text-red-600">{{ doubleCount }}</strong>
     </p>
     <ion-button @click="increment">Increment</ion-button>
