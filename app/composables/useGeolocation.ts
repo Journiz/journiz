1ccfc7237ab06a95179afa94c5b9876423b16e5f
leoboyerbx <contact@leoboyer.fr>
2023-03-14 23:32:47 +0100

    style: apply linter to geoloc composable

--- app/composables/useGeolocation.ts
+++ app/composables/useGeolocation.ts
@@ -1,65 +1,68 @@
-import {registerPlugin} from "@capacitor/core";
+import { registerPlugin } from '@capacitor/core'
+import { reactive } from 'vue'
+import { BackgroundGeolocationPlugin } from '@capacitor-community/background-geolocation'
 
-const BackgroundGeolocation = registerPlugin("BackgroundGeolocation") as BackgroundGeolocationPlugin;
-import {reactive} from 'vue'
-import {BackgroundGeolocationPlugin} from '@capacitor-community/background-geolocation';
+const BackgroundGeolocation = registerPlugin(
+  'BackgroundGeolocation'
+) as BackgroundGeolocationPlugin
 
-const currentLocation = reactive({lng: 6.129384, lat: 45.899247})
+const currentLocation = reactive({ lng: 6.129384, lat: 45.899247 })
 let watcherId: string | null = null
 
 const startWatching = () => {
-    if (watcherId) return
-    BackgroundGeolocation.addWatcher(
-        {
-            backgroundMessage: "Cancel to prevent battery drain.",
-            backgroundTitle: "Tracking You.",
-            requestPermissions: true,
-        },
-        function callback(location, error) {
-            if (error) {
-                if (error.code === "NOT_AUTHORIZED") {
-                    if (window.confirm(
-                        "This app needs your location, " +
-                        "but does not have permission.\n\n" +
-                        "Open settings now?"
-                    )) {
-                        // It can be useful to direct the user to their device's
-                        // settings when location permissions have been denied. The
-                        // plugin provides the 'openSettings' method to do exactly
-                        // this.
-                        BackgroundGeolocation.openSettings();
-                    }
-                }
-                return console.error(error);
-            }
-            if (location) {
-                currentLocation.lng = location.longitude
-                currentLocation.lat = location.latitude
-            } else {
-                console.error('Location object from OS was empty')
-            }
-            return '' // console.log(location);
+  if (watcherId) return
+  BackgroundGeolocation.addWatcher(
+    {
+      backgroundMessage: 'Cancel to prevent battery drain.',
+      backgroundTitle: 'Tracking You.',
+      requestPermissions: true,
+    },
+    function callback(location, error) {
+      if (error) {
+        if (error.code === 'NOT_AUTHORIZED') {
+          if (
+            window.confirm(
+              'This app needs your location, ' +
+                'but does not have permission.\n\n' +
+                'Open settings now?'
+            )
+          ) {
+            // It can be useful to direct the user to their device's
+            // settings when location permissions have been denied. The
+            // plugin provides the 'openSettings' method to do exactly
+            // this.
+            BackgroundGeolocation.openSettings()
+          }
         }
-    ).then(function after_the_watcher_has_been_added(watcher_id) {
-        // When a watcher is no longer needed, it should be removed by calling
-        // 'removeWatcher' with an object containing its ID.
-        /* BackgroundGeolocation.removeWatcher({
-          id: watcher_id
-        }); */
-        watcherId = watcher_id
-    });
+        return console.error(error)
+      }
+      if (location) {
+        currentLocation.lng = location.longitude
+        currentLocation.lat = location.latitude
+      } else {
+        console.error('Location object from OS was empty')
+      }
+      return '' // console.log(location);
+    }
+  ).then(function watcherAdded(id: string) {
+    // When a watcher is no longer needed, it should be removed by calling
+    // 'removeWatcher' with an object containing its ID.
+    /* BackgroundGeolocation.removeWatcher({
+      id: watcher_id
+    }); */
+    watcherId = id
+  })
 }
 
 const stopWatching = () => {
-    if (watcherId) {
-        BackgroundGeolocation.removeWatcher({
-            id: watcherId
-        });
-        watcherId = null
-    }
+  if (watcherId) {
+    BackgroundGeolocation.removeWatcher({
+      id: watcherId,
+    })
+    watcherId = null
+  }
 }
 
-
 export const useGeolocation = () => {
-    return {startWatching, stopWatching, currentLocation}
+  return { startWatching, stopWatching, currentLocation }
 }
