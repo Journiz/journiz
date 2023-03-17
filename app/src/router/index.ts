c3214f81f5f2fbd6c52b3e9fc43161df21518348
leoboyerbx <contact@leoboyer.fr>
2023-03-17 08:09:01 +0100

    wip:  app migration to vue

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,23 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from '@ionic/vue-router'
 import HomeView from '../views/HomeView.vue'
+import PocGeolocation from '~/views/PocGeolocation.vue'
+import Notif from '~/views/Notif.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/home',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/geolocation',
+      name: 'geolocation',
+      component: PocGeolocation,
+    },
+    {
+      path: '/notif',
+      name: 'notif',
+      component: Notif,
+    },
+  ],
 })
 
 export default router
