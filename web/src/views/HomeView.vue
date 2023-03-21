3d73f9cf630958b378968d0b1bb975ecdd46d33d
Vincent <vincecreton@hotmail.fr>
2023-03-21 13:26:27 +0100

    FEATURE: Layout general with sidebar

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import HelloBackoffice from '~/components/HelloBackoffice.vue'
 import Sidebar from '~/components/Sidebar.vue'
 </script>
 
 <template>
-  <main class="bg-primary-200 h-full w-full flex">
-    <HelloBackoffice /><Sidebar />
+  <main class="bg-primary-200 w-screen h-screen flex">
+    <Sidebar />
   </main>
 </template>
