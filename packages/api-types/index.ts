67661cd56b0cd80ec12b0cc7ab6810afbecaf89c
leoboyerbx <contact@leoboyer.fr>
2023-03-24 13:12:26 +0100

    Simplify exports

--- packages/api-types/index.ts
+++ packages/api-types/index.ts
@@ -1,8 +1,7 @@
 export * from './schemas/Journey'
 export * from './schemas/Team'
 export * from './schemas/Trip'
-export { PointSchema } from './schemas/Point'
-export type { Point } from './schemas/Point'
+export * from './schemas/Point'
 
 export { default as cloneDeep } from './utils/cloneDeep'
 export { flattenExpands } from './utils/flattenExpands'
