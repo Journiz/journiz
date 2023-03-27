7242825fc97fa985fd7bbc8edf0d68fa8fcc267c
leoboyerbx <contact@leoboyer.fr>
2023-03-27 17:35:01 +0200

    chore: clean backoffice loads

--- web/src/components/HelloBackoffice.vue
+++ web/src/components/HelloBackoffice.vue
@@ -1,11 +1,4 @@
-<script lang="ts" setup>
-import { useRealtimeJourney } from '@journiz/composables'
-
-const { data: journey } = useRealtimeJourney('r6t26b2ariy34km')
-</script>
+<script lang="ts" setup></script>
 <template>
-  <div class="">
-    Hello Journiz backoffice
-    <pre>{{ journey }}</pre>
-  </div>
+  <div class="">Hello Journiz backoffice</div>
 </template>
