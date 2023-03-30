99eecf5505f31b6cb6e20b95906d37d8776f6760
leoboyerbx <contact@leoboyer.fr>
2023-03-31 00:19:46 +0200

    feat:  update trip schema to match api

--- packages/api-types/schemas/Trip.ts
+++ packages/api-types/schemas/Trip.ts
@@ -6,9 +6,11 @@ export const TripSchema = z.object({
   created: z.string(),
   date: z.string(),
   id: z.string(),
+  shortId: z.string(),
   journey: z.string(),
   updated: z.string(),
   name: z.string(),
+  status: z.enum(['pairing', 'playing', 'finished']),
   expand: z
     .object({
       teams: z.array(TeamSchema).optional(),
