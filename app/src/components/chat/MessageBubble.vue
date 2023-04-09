3bdc4381ffbdd0305ecc2e18ca91956521b04e35
leoboyerbx <contact@leoboyer.fr>
2023-04-09 16:20:18 +0200

    feat: read and unread messages checkmark

--- app/src/components/chat/MessageBubble.vue
+++ app/src/components/chat/MessageBubble.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import type { Message as MessageType } from '@journiz/api-types'
+import { IonIcon } from '@ionic/vue'
+import { checkmarkCircleOutline, ellipseOutline } from 'ionicons/icons'
 
 const props = defineProps<{
   message: MessageType
@@ -18,10 +20,15 @@ onMounted(() => {
 <template>
   <div
     ref="el"
-    class="bubble rounded-xl p-4 text-white my-2.5 mx-5 whitespace-pre-wrap"
+    class="bubble rounded-xl p-4 text-white my-2.5 mx-5 whitespace-pre-wrap relative"
     :class="message.sender === userType ? 'bg-blue-500 ml-auto' : 'bg-blue-400'"
   >
-    {{ message.content }}
+    <span>
+      {{ message.content }}
+    </span>
+    <div v-if="message.sender === userType" class="absolute bottom-0 right-1">
+      <IonIcon :icon="message.read ? checkmarkCircleOutline : ellipseOutline" />
+    </div>
   </div>
 </template>
 
