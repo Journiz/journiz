9e7fdc11b60d1a89c9abc9f5a617e713728a7c6b
leoboyerbx <contact@leoboyer.fr>
2023-04-09 11:11:50 +0200

    wip: fix message bubble new schema

---
+++ app/src/components/chat/MessageBubble.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Message as MessageType } from '../../../../packages/api-types'
+
+defineProps<{ message: MessageType; userType: MessageType['sender'] }>()
+</script>
+
+<template>
+  <div
+    class="bubble rounded-xl p-4 text-white my-2.5 mx-5"
+    :class="message.sender === userType ? 'bg-blue-500 ml-auto' : 'bg-blue-400'"
+  >
+    {{ message.content }}
+  </div>
+</template>
+
+<style>
+.bubble {
+  max-width: 69.7%;
+}
+</style>
