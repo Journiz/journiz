76d9b169c723c12de07791b14717e87509029b8a
leoboyerbx <contact@leoboyer.fr>
2023-04-12 16:23:10 +0200

    feat: user list journeys

--- web/src/views/dashboard/journey/JourneysListView.vue
+++ web/src/views/dashboard/journey/JourneysListView.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import JourneyLine from '~/components/JourneyLine.vue'
+import { useJourneys } from '@journiz/composables'
 import PageTitle from '~/components/PageTitle.vue'
 import DefaultButton from '~/components/DefaultButton.vue'
+import { useUserStore } from '~/stores/user'
+import JourneyItem from '~/components/JourneyItem.vue'
 const router = useRouter()
-const testAction = () => {
-  console.log('🌼 Click on button')
+
+const userStore = useUserStore()
+
+const { data: journeys } = useJourneys({
+  filter: `user="${userStore.user!.id}"`,
+  sort: '-created',
+})
+
+const deleteJourney = (id: string) => {
+  console.log('delete journey', id)
 }
 </script>
 
@@ -15,9 +25,18 @@ const testAction = () => {
     <default-button class="mb-6" @click="router.push('parcours/creation')"
       >Nouveau parcours</default-button
     >
-    <JourneyLine
-      journey-name="Sortie à Grenoble"
-      basecamp-name="Hôtel de ville"
-    />
+    <div class="flex flex-col gap-4">
+      <JourneyItem
+        v-for="journey in journeys"
+        :key="journey.id"
+        :journey="journey"
+        @delete-journey="deleteJourney(journey.id)"
+        @edit-journey="router.push('/dashboard/parcours/' + journey.id)"
+      />
+    </div>
+    <!--    <JourneyLine-->
+    <!--      journey-name="Sortie à Grenoble"-->
+    <!--      basecamp-name="Hôtel de ville"-->
+    <!--    />-->
   </article>
 </template>
