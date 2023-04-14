c3de7707c110c52b924a841cd5297109a0d751c6
Vincent Creton <v.creton@herewecom.fr>
2023-04-14 09:47:33 +0200

    Add gl-draw dependency

---
+++ web/src/components/MapWithSafeZoneSearch.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+import Map from '~/components/Map.vue'
+import { Coordinates } from '~/types/Coordinates'
+defineProps({
+  mapCenter: {
+    type: Object as PropType<Coordinates>,
+    required: true,
+  },
+  zoom: {
+    type: Number as PropType<number>,
+    default: 10,
+  },
+})
+</script>
+<template>
+  <div><Map :map-center="mapCenter" :zoom="zoom" /></div>
+</template>
