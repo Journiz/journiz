17d2ceaa2114850ce4132ed89717403f0c3aaa8f
Vincent Creton <v.creton@herewecom.fr>
2023-04-26 09:19:37 +0200

    wip editor view

--- web/src/views/dashboard/journey/JourneyEditorView.vue
+++ web/src/views/dashboard/journey/JourneyEditorView.vue
@@ -1,15 +1,46 @@
 <script lang="ts" setup="">
 import { useRouter } from 'vue-router'
-import PointList from '~/components/PointList.vue'
-// import JourneyData from '~/components/journey/JourneyData.vue'
+import { computed } from 'vue'
+import PointList from '~/components/point/PointList.vue'
+import CustomHeader from '~/components/layout/CustomHeader.vue'
+import { useJourneyStore } from '~/stores/journey'
+import DefaultButton from '~/components/buttons/DefaultButton.vue'
+import Map from '~/components/Map.vue'
+import MapMarker from '~/components/MapMarker.vue'
 
 const router = useRouter()
+const store = useJourneyStore()
+const mapCenter = computed(() => {
+  return [store.journey!.basecampLongitude, store.journey!.basecampLatitude]
+})
 </script>
 <template>
-  <div>
-    <div>Ici l'éditeur avec la liste des points etc</div>
-    <button @click="router.push({ name: 'export-journey' })">Export</button>
-    <PointList />
-    <!-- <JourneyData /> -->
+  <div class="flex flex-col h-full">
+    <CustomHeader
+      :title="store.journey.name"
+      :no-display-return="true"
+      class="px-16 h-auto mb-7 pt-12"
+    >
+      <defaultButton @click="router.push({ name: 'export-journey' })">
+        Exporter
+      </defaultButton>
+    </CustomHeader>
+    <div class="px-16 flex flex-grow pb-8 overflow-hidden">
+      <PointList class="w-1/2 pr-2 max-h-full overflow-scroll" />
+      <div class="relative flex-grow">
+        <Map
+          class="w-1/2 rounded-xl overflow-hidden"
+          :map-center="mapCenter"
+          :zoom="14"
+        >
+          <MapMarker :position="mapCenter" icon="basecamp" />
+          <MapMarker
+            v-for="point in store.journey.expand.points"
+            :position="[point.longitude, point.latitude]"
+            icon="basic"
+          />
+        </Map>
+      </div>
+    </div>
   </div>
 </template>
