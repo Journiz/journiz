2f78a8350883f3f220ae2d80e0afbf1bceaaacf8
leoboyerbx <contact@leoboyer.fr>
2023-05-04 20:32:08 +0200

    wip design system

--- app/src/components/HelloJourniz.vue
+++ app/src/components/HelloJourniz.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import logo from '~/assets/logo/text.svg?raw'
+import Button from '~/components/design-system/Button.vue'
 </script>
 <template>
   <div
@@ -9,5 +10,8 @@ import logo from '~/assets/logo/text.svg?raw'
     <p class="text-center px-4 font-light">
       Rejoignez votre prochain parcours dès maintenant !
     </p>
+    <div class="flex flex-col gap-4">
+      <Button>Je suis un participant</Button>
+    </div>
   </div>
 </template>
--- app/src/components/design-system/Button.vue
+++ app/src/components/design-system/Button.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ButtonHTMLAttributes, PropType, ref } from 'vue'
 // @ts-ignore
-import SvgSpinners180Ring from '~icons/svg-spinners/180-ring'
+import { useButtonDisabled } from '@journiz/composables'
 import { ButtonColor, ButtonVariant } from '~/types/Button'
 
 const props = defineProps({
   color: {
     type: String as PropType<ButtonColor>,
-    default: 'primary',
+    default: 'violet',
   },
   variant: {
     type: String as PropType<ButtonVariant>,
@@ -32,16 +32,66 @@ const { actualDisabled } = useButtonDisabled(props)
 </script>
 <template>
   <button
-    class="btn px-6 py-3 rounded-lg text-white w-fit transition-all flex items-center gap-2"
+    class="btn px-6 py-3 rounded-lg text-white w-fit transition-all flex items-center gap-2 btn-animation"
     cursor="pointer disabled:not-allowed"
-    :class="[
-      { 'btn-animation': hasBeenClicked },
-      `btn-${variant} btn-${color}`,
-    ]"
     :disabled="actualDisabled"
     @mousedown="hasBeenClicked = true"
   >
-    <SvgSpinners180Ring v-show="loading" />
+    <!--    <SvgSpinners180Ring v-show="loading" />-->
+    <div class="i-"></div>
     <slot />
   </button>
 </template>
+<style scoped lang="scss">
+.btn {
+  border: solid 1px transparent;
+  --disabled-color: theme('colors.gray.300');
+  --disabled-text: theme('colors.gray.100');
+
+  // Colors
+  &-theme {
+    --color: theme('colors.theme.DEFAULT');
+  }
+  &-blue {
+    --color: theme('colors.blue');
+  }
+  &-green {
+    --color: theme('colors.green.DEFAULT');
+  }
+  &-red {
+    --color: theme('colors.red');
+  }
+  &-yellow {
+    --color: theme('colors.yellow');
+  }
+  &-violet {
+    --color: theme('colors.violet');
+  }
+
+  // Variants
+  &-fill {
+    background-color: var(--color);
+    border-color: var(--color);
+    color: white;
+    &:disabled {
+      background-color: var(--disabled-color);
+      border-color: var(--disabled-color);
+      color: var(--disabled-text);
+    }
+  }
+
+  &-outline {
+    border: solid var(--color) 1px;
+    color: var(--color);
+    &:hover {
+      background-color: var(--color);
+      color: white;
+    }
+    &:disabled {
+      background-color: var(--disabled-color);
+      color: var(--disabled-text);
+      border-color: var(--disabled-color);
+    }
+  }
+}
+</style>
