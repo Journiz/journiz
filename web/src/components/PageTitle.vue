2a1ad35a16f003f72ca8d5cd111b2b6fa9549ddf
Vincent Creton <v.creton@herewecom.fr>
2023-05-15 09:09:09 +0200

    WIP: PageTitle component

--- web/src/components/PageTitle.vue
+++ web/src/components/PageTitle.vue
@@ -1,4 +1,15 @@
 <script setup></script>
 <template>
-  <h1 class="text-blue-900 font-extrabold text-3xl"><slot /></h1>
+  <h1 class="relative text-blue-900 font-extrabold text-3xl"><slot /></h1>
 </template>
+<style scoped>
+h1::after {
+  content: url('~/assets/images/svg/underline-h1.svg');
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: auto;
+  transform: translateY(50%);
+}
+</style>
