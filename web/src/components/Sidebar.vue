2f106bc560a76d101e108d479fac49f7b3a4f2b5
Vincent Creton <v.creton@herewecom.fr>
2023-05-15 08:54:09 +0200

    FEATURE: Navbar

--- web/src/components/Sidebar.vue
+++ web/src/components/Sidebar.vue
@@ -13,8 +13,15 @@ const logout = async () => {
 </script>
 
 <template>
-  <div class="relative bg-green-dark h-full w-47 pt-24 pb-10">
-    <div class="flex flex-wrap items-end justify-end w-content">
+  <div
+    class="relative bg-green-dark h-full w-47 pt-41 pb-10 flex flex-col justify-between"
+  >
+    <img
+      class="absolute bottom-0 left-0 right-0 object-contain"
+      src="../assets/images/svg/bg-navbar.svg"
+      alt="Navbar background"
+    />
+    <div class="relative flex flex-wrap items-end justify-end w-content">
       <SidebarMenuItem
         icon="journey"
         name="Vos parcours"
@@ -25,17 +32,20 @@ const logout = async () => {
         name="Modèles"
         path="/dashboard/communaute"
       />
-      <!-- <button class="min-w-41" @click="logout">
-        <div class="flex flex-row text-white-off items-center">
-          <icon-signout class="sidebar-icon mr-2" />
-          <span class="text-xs">Déconnexion</span>
-        </div>
-      </button> -->
     </div>
-    <img
-      class="absolute bottom-0 left-0 right-0 object-contain"
-      src="../assets/images/svg/bg-navbar.svg"
-      alt="Navbar background"
-    />
+    <div class="relative px-4">
+      <div
+        class="text-center p-2 bg-white-off rounded-lg text-green-dark font-black text-lg"
+      >
+        {{ userStore.user.username }}
+      </div>
+      <div
+        class="flex text-white-off font-extralight items-center justify-center mt-5 text-md cursor-pointer"
+        @click="logout"
+      >
+        <IconSignout class="mr-1" />
+        Se déconnecter
+      </div>
+    </div>
   </div>
 </template>
