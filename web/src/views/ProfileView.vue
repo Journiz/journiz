7131a8a6fea8d1b896e1afc301a95c090753033f
leoboyerbx <contact@leoboyer.fr>
2023-05-16 13:59:00 +0200

    feat: profile route

---
+++ web/src/views/ProfileView.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup="">
+import PageTitle from '~/components/PageTitle.vue'
+</script>
+<template>
+  <article class="pt-10 px-16">
+    <page-title class="mb-10">Mon profil</page-title>
+  </article>
+</template>
