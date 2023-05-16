18f12a412957413fadef3e4fdc644f51d6b5c140
leoboyerbx <contact@leoboyer.fr>
2023-05-16 17:24:58 +0200

    wip: takenColors in customize colors

--- app/src/components/team/customize/CustomizeColor.vue
+++ app/src/components/team/customize/CustomizeColor.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup="">
+import { computed } from 'vue'
 import { useTeamStore } from '~/stores/team/team'
 
 const store = useTeamStore()
+const takenColors = computed(
+  () =>
+    store.trip?.expand?.teams
+      ?.filter((team) => team.id !== store.team?.id && team.color)
+      .map((team) => team.color) ?? []
+)
 </script>
 <template>
   <div class="bg-blue">
     <div>Color: {{ store.team.color }}</div>
-    <pre>{{ store.trip.expand.teams }}</pre>
+    <pre>{{ takenColors }}</pre>
   </div>
 </template>
