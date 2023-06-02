ef354b1368b84c250f240789745f16d4c6b24afa
leoboyerbx <contact@leoboyer.fr>
2023-06-02 11:11:11 +0200

    wip: modal

---
+++ app/src/components/modal/AppModal.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup="">
+import { ref } from 'vue'
+import Modal from '~/components/modal/Modal.vue'
+import Button from '~/components/design-system/Button.vue'
+const title = ref('titre')
+const text = ref(
+  `Gardez toujours un œil sur votre environnement et assurez-vous d'être à l'arrêt en toute sécurité avant de consulter votre téléphone.`
+)
+</script>
+<template>
+  <Modal>
+    <div class="w-full flex flex-col gap-6">
+      <h1 class="text-base font-black m-0 text-center">{{ title }}</h1>
+      <div class="w-full bg-red h-36"></div>
+      <p class="font-light text-sm text-center">{{ text }}</p>
+      <div class="flex flex-col w-full">
+        <Button>Hey</Button>
+      </div>
+    </div>
+  </Modal>
+</template>
---
+++ app/src/components/modal/Modal.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup="">
+import { onMounted, ref, watch } from 'vue'
+
+const isOpen = ref(true)
+const dialog = ref()
+const updateOpen = () => {
+  if (isOpen.value) {
+    dialog.value.showModal()
+  } else {
+    dialog.value.close()
+  }
+}
+watch(isOpen, updateOpen)
+onMounted(() => {
+  updateOpen()
+})
+const closeModal = () => {
+  isOpen.value = false
+}
+</script>
+<template>
+  <dialog ref="dialog">
+    <slot :close-modal="closeModal"></slot>
+  </dialog>
+</template>
+<style scoped lang="scss">
+dialog {
+  @apply outline-none bg-white rounded-lg w-full px-6;
+  &::backdrop {
+    @apply bg-black/30;
+  }
+}
+</style>
