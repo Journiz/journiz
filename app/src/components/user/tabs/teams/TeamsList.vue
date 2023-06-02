ddb2097f027e9ffc8504b48e08a60c3086471573
leoboyerbx <contact@leoboyer.fr>
2023-06-02 10:13:48 +0200

    Teak

--- app/src/components/user/tabs/teams/TeamsList.vue
+++ app/src/components/user/tabs/teams/TeamsList.vue
@@ -7,5 +7,8 @@ const teams = computed(() => store.trip?.expand?.teams ?? [])
 // https://www.notion.so/journiz/Affichage-quipe-en-liste-quipe-single-69aebac3a6a54789a79fbf348b5c8e2d?pvs=4
 </script>
 <template>
-  <div v-if="teams">Ici les équipes, by Vincent®</div>
+  <div v-if="teams" class="bg-beige h-full">
+    Ici les équipes, by Vincent® (attention au padding de la bottom bar, le
+    composant passe derrière)
+  </div>
 </template>
