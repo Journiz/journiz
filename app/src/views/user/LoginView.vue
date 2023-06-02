fc92d980310728249a2be2d8d87276b46676ce5e
Vincent Creton <v.creton@herewecom.fr>
2023-06-02 11:41:42 +0200

    FEATURE: Login view

--- app/src/views/user/LoginView.vue
+++ app/src/views/user/LoginView.vue
@@ -8,6 +8,10 @@ import {
 } from '@ionic/vue'
 import { ref } from 'vue'
 import { useUserStore } from '~/stores/user'
+import TextInput from '~/components/forms/TextInput.vue'
+import Page from '~/components/Page.vue'
+import Header from '~/components/design-system/Header.vue'
+import Button from '~/components/design-system/Button.vue'
 
 const store = useUserStore()
 const router = useIonRouter()
@@ -27,30 +31,36 @@ const login = async () => {
 }
 </script>
 <template>
-  <IonPage>
-    <IonHeader class="">
-      <IonToolbar>
-        <IonTitle>Se connecter</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent :fullscreen="true">
-      <ion-item>
-        <ion-label>Email</ion-label>
-        <ion-input v-model="email" type="email"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label>Mot de passe</ion-label>
-        <ion-input
-          v-model="password"
-          placeholder=""
-          type="password"
-        ></ion-input>
-      </ion-item>
+  <Page class="justify-center h-full">
+    <img
+      class="absolute h-full w-full top-0 left-0 object-cover object-center"
+      src="../../assets/img/backgrounds/bg-login.jpg"
+      alt="Background"
+    />
+    <div class="px-8">
+      <h2
+        class="relative color-green-dark text-2xl font-black text-center mb-10"
+      >
+        Connexion
+      </h2>
+      <TextInput
+        v-model="email"
+        class="mb-4"
+        label="Identifiant"
+        svg-name="people"
+        :no-display-required-stars="true"
+      />
+      <TextInput
+        v-model="password"
+        label="Mot de passe"
+        type="password"
+        svg-name="lock"
+        :no-display-required-stars="true"
+      />
       <p v-if="error" class="text-red-500 p-2">{{ error }}</p>
-      <ion-button :disabled="loading" @click="login">
-        <ion-spinner v-if="loading" name="dots"></ion-spinner>
-        <span v-else>Connexion</span>
-      </ion-button>
-    </IonContent>
-  </IonPage>
+      <Button class="relative w-full mt-6" color="theme" @click="login"
+        >Connexion</Button
+      >
+    </div>
+  </Page>
 </template>
