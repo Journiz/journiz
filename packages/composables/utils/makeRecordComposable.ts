5ee707df7e0cfb30962ad09f9a7f6d99cc8e8830
leoboyerbx <contact@leoboyer.fr>
2023-06-17 22:22:04 +0200

    clean: remove unnecessary deep watcher in makeRecordComposable

--- packages/composables/utils/makeRecordComposable.ts
+++ packages/composables/utils/makeRecordComposable.ts
@@ -76,7 +76,7 @@ export function makeRecordComposable<Schema extends ZodType<any>>(
       }
     }
     // When the raw data changes, update the typed data
-    watch(rawData, parseData, { deep: true })
+    watch(rawData, parseData)
 
     const refresh = async () => {
       const id = idRef.value
